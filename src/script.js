b01f4c6a080a853a6760bdf89f71423c0374b26b
gizembceylan <78044064+gizembceylan@users.noreply.github.com>
2022-01-04 22:23:43 +0100

    js_time

--- src/script.js
+++ src/script.js
@@ -1,3 +1,15 @@
+function formatDate(timestamp){
+    //calculate the date
+    let date = new Date(timestamp);
+    let hours= date.getHours();
+    let minutes = date.getMinutes();
+    let days= ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"]
+    let day= days[date.getDay()];
+
+    return '${day} ${hours}';
+} 
+ 
+ 
  function displayTemperature(response){
      console.log(response.data.main.temp);
      let temperatureElement = document.querySelector("#temperature");
@@ -5,12 +17,16 @@
     let descriptionElement = document.querySelector("#description");
      let humidityElement = document.querySelector("#humidity");
      let windElement = document.querySelector("#wind");
+    
      temperatureElement.innerHTML = Math.round(response.data.main.temp);
      cityElement.innerHTML= response.data.name;
      descriptionElement.innerHTML= response.data.weather[0].description;
      humidityElement.innerHTML = response.data.main.humidity;
-     windElement.innerHTML = Math.round(response.data.main.speed);
+     windElement.innerHTML = formatDate(response.data.wind.speed);
      
+     dateElement = document.querySelector("#date");
+     dateElement.innerHTML = Math.round(response.data.dt*1000)
+    
  }
  
 let apiKey = "1969214d93e86fb47c6d955926f85399";
