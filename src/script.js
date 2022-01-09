540a72dcd44ebac9e5f09ab0ce6bb5f7df9e4470
gizembceylan <78044064+gizembceylan@users.noreply.github.com>
2022-01-09 14:13:45 +0100

    js-forecast-problemsolved

--- src/script.js
+++ src/script.js
@@ -58,7 +58,9 @@ function getForecast(coordinates){
   console.log(coordinates);
   let apiKey = "1969214d93e86fb47c6d955926f85399";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
+ 
+  console.log(apiUrl);
+axios.get(apiUrl).then(displayForecast);
 }
  
  function displayTemperature(response){
