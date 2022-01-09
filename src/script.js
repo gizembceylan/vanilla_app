d52903f41d9b7249c92961547511b2c9fcb7eefb
gizembceylan <78044064+gizembceylan@users.noreply.github.com>
2022-01-09 15:01:29 +0100

    forecast-6days

--- src/script.js
+++ src/script.js
@@ -18,35 +18,44 @@ function formatDate(timestamp) {
 
     return `${day} ${hours}:${minutes}`; 
 } 
- 
+
+function formatDay(timestamp){
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
 
 
-  let days = ["Thu","Fri","Sat","Sun"];
   let forecastHTML =   `<div class="row">`;
-  days.forEach(function (day){
+  forecast.forEach(function (forecastDay, index){
+    if (index< 6) {
     forecastHTML = forecastHTML + 
   `
   <div class="col-2">
-  <div class="weather-forecast-date">${day}</div>
+  <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
     <img
-        src="https://ssl.gstatic.com/onebox/weather/48/sunny.png"
+        src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
         alt=""
         width="36" />
 
         <div class="weather-forecast-temperatures">
         <span class="weather-forecast-temperature-max">
-          18°
+          ${Math.round(forecastDay.temp.max)}
         </span>
         <span class="weather-forecast-temperature-min">
-          12°
+          ${Math.round(forecastDay.temp.min)}
         </span>
       </div>
         </div>
        `;
+    }
 
   });
 
