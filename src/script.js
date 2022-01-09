bc134fabb010fd0bf3da0afaa78012302295ec81
gizembceylan <78044064+gizembceylan@users.noreply.github.com>
2022-01-09 13:51:09 +0100

    js_problem

--- src/script.js
+++ src/script.js
@@ -19,11 +19,14 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`; 
 } 
  
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
-  let forecastHTML =   `<div class="row">`;
-  let days = ["Thu","Fri","Sat","Sun"];
+function displayForecast(response) {
+  console.log(response.data.daily);
 
+  let forecastElement = document.querySelector("#forecast");
+
+
+  let days = ["Thu","Fri","Sat","Sun"];
+  let forecastHTML =   `<div class="row">`;
   days.forEach(function (day){
     forecastHTML = forecastHTML + 
   `
@@ -49,6 +52,13 @@ function displayForecast() {
 
 forecastHTML = forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiKey = "1969214d93e86fb47c6d955926f85399";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
  
  function displayTemperature(response){
@@ -76,6 +86,8 @@ celciusTemperature = response.data.main.temp;
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(response.data.coord);
  }
 
 
@@ -130,5 +142,3 @@ celciusLink.addEventListener("click", displayCelciusTemperature);
 
 
  search("New York");
-
-  displayForecast();
